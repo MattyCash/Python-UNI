6c4ce7eb2944631fa58b58f190a2a1bf65682497
Adil <107040452+MattyCash@users.noreply.github.com>
2023-09-13 11:29:37 +0600

    Add files via upload

---
+++ CL2.py
@@ -0,0 +1,79 @@
+#1
+# print(4, 8, 15, 16, 32, 42, sep=' ')
+
+#2
+# print(4, 8, 15, 16, 32, 42, sep='\n')
+
+#3
+# print("*", "**", "***", "****", "*****","******","*******", sep='\n')
+
+#4
+# a = int(input())
+# print(a, a+1 , a+2, sep='\n')
+
+#5
+# a = (int(input()))
+# b = (int(input()))
+# c = (int(input()))
+# print (a+b+c)
+
+#6
+# a = (int(input()))
+# print(a*a*a)
+# print(6*a*a)
+
+#7
+# a = (int(input()))
+# print("Next ", a+1)
+# print("Before ", a-1)
+
+#8
+# a = (int(input()))
+# b = (int(input()))
+# c = (int(input()))
+# d = (int(input()))
+# print((a+b+c+d)*3)
+
+#9
+# a = (int(input()))
+# b = (int(input()))
+#
+# print((a, " + ", b, " = ", a+b), (a, " - ", b, " = ", a-b), (a, " * ", b, " = ", a*b), sep='\n')
+
+#10
+# a = (int(input()))
+# print(a//100)
+
+#11
+# a = (int(input()))
+# b = (int(input()))
+# c = 0
+#
+# print(b//a)
+# print(b%a)
+
+#12
+# a = (int(input()))
+#
+# print(a//2 + 1)
+
+#13
+# a = (int(input()))
+# b = a % 60
+# c = a // 60
+# print(c, b)
+
+#14
+# a = (int(input()))
+# first = a // 100
+# last = a % 100 % 10
+# second = (a % 100 - last) / 10
+# print(int(first + last + second), int(first * second * last))
+
+#15
+# a = (int(input()))
+# f = a // 1000
+# first = a // 100 % 10
+# last = a % 100 % 10
+# second = (a % 100 - last) / 10
+# print(f, first, int(second), last, sep='\n')
